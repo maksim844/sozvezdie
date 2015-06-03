297a7de630c81ad5908995e0664fd20e9c941725
Maks <mb@droogc.ru>
2015-06-03 21:25:38 +0300

    fix

--- app/models/node.rb
+++ app/models/node.rb
@@ -4,7 +4,7 @@ class Node < ActiveRecord::Base
   has_many :teachers
   has_many :albums
   has_many :articles
-  has_many :main
+  has_many :news
   attr_accessible :ancestry, :name, :position, :content, :url, :description, :keywords
 
   #default_scope where("in_site_map = 1 or content_producer like '%Main%'")
